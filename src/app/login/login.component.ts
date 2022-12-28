f07e50f770da5eb9e76f410fe6e53b07c62aa012
Selim.Reza <selim.reza>
2022-12-28 14:15:06 +0600

    updates on login component ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,7 @@
 import { Router } from '@angular/router';
 import { AuthService } from './../service/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective, NgForm, Validators } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class LoginErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 
 @Component({
@@ -39,7 +29,6 @@ export class LoginComponent implements OnInit {
       "password": new FormControl('', [Validators.required, Validators.minLength(6)]),
     }
   );
-  matcher = new LoginErrorStateMatcher();
 
   constructor(
     private auth: AuthService,

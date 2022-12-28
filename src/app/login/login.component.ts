32651b1c92015bfd06744fc6602789a0fabe3c02
Selim.Reza <selim.reza>
2022-12-28 10:00:01 +0600

    updates on login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -59,21 +59,5 @@ export class LoginComponent implements OnInit {
 
   }
 
-  login() {
-    if (this.username.trim().length === 0) {
-      this.errorMsg = "Username is required";
-    } else if (this.password.trim().length === 0) {
-      this.errorMsg = "Password is required";
-    } else {
-      this.errorMsg = "";
-      // let res = this.auth.login(this.username, this.password);
-      // if (res === 200) {
-      //   this.router.navigate(['home']);
-      // }
-      // if (res === 403) {
-      //   this.errorMsg = "Invalid Credentials";
-      // }
-    }
-  }
 
 }
